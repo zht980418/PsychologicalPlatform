e1973d45abfd287785a4e3b0caa7a2fbdd1feb01
yichen <yichen97.dai@gmail.com>
2021-01-18 17:17:20 +0800

    2021_1_12

--- qblog-client/src/views/Quiz.vue
+++ qblog-client/src/views/Quiz.vue
@@ -1,20 +1,34 @@
 <template>
-  <el-container style="height: 500px; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-  <el-menu  router>
-    <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
-        {{ item.navItem }}
-    </el-menu-item>
-  </el-menu>
-  </el-aside>
-  <el-main>
-    <router-view ></router-view>
-  </el-main>
-</el-container>
+  <el-row>
+    <el-row>
+      <Header></Header>
+      <el-divider></el-divider>
+    </el-row>
+
+    <el-container style="height: 600px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu  router>
+          <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+            {{ item.navItem }}
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view ></router-view>
+      </el-main>
+    </el-container>
+
+    <el-row><Footer></Footer></el-row>
+  </el-row>
+
 </template>
 
 <script>
   export default {
+    components: {
+      Footer: ()=>import("@/components/Footer"),
+      Header:() => import("@/components/Header"),
+    },
     data() {
       return {
         navList:[
