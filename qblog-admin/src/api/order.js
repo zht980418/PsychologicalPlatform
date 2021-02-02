e9922650d04afc5ea05cc615d5094271cd4bbefd
1010yyn <yangyiling1997@yeah.net>
2021-02-02 23:27:12 +0800

    2.2 api Update

--- qblog-admin/src/api/order.js
+++ qblog-admin/src/api/order.js
@@ -1,13 +1,28 @@
 import request from '@/utils/request'
 
+// 获取限制信息
 export function getConstraint(params) {
   return request({
     url: '/vue-admin-template/order/list',
     method: 'get',
-    params
+    data: {
+      doctorId: params
+    }
   })
 }
 
+// 获取预约信息
+export function getOrder(params) {
+  return request({
+    url: '/vue-admin-template/order/list',
+    method: 'get',
+    data: {
+      doctorId: params
+    }
+  })
+}
+
+// 存储预约信息
 export function postOrder(params) {
   return request({
     url: '/vue-admin-template/order/list',
