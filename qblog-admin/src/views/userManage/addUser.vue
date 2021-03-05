d3c1d7e97b627c71cb2cf78c93bedb5a24615310
yichen <yichen97.dai@gmail.com>
2021-03-05 19:58:51 +0800

    Update addUser.vue
    表单校验

--- qblog-admin/src/views/userManage/addUser.vue
+++ qblog-admin/src/views/userManage/addUser.vue
@@ -9,11 +9,11 @@
         </div>
         <div class="user-info">
           <div class="user-info user-info-section">
-            <el-form class="info-form" inline label-position="left" label-width="80px">
-              <el-form-item label="用户名" inline-message>
-                <el-input v-model="user.username"/>
+            <el-form class="info-form" inline :model="user" :rules="userRules" ref="user" label-position="left" label-width="80px">
+              <el-form-item label="用户名" prop="username" inline-message>
+                <el-input ref="username" v-model="user.username"/>
               </el-form-item>
-              <el-form-item label="密码" inline-message>
+              <el-form-item ref="password" label="密码" prop="password" inline-message>
                 <el-input v-model="user.password"/>
               </el-form-item>
               <el-form-item label="姓名" inline-message>
@@ -30,7 +30,7 @@
                 </el-checkbox-group>
               </el-form-item>
               <el-row class="btn">
-                <el-button type="primary" @click="onSubmit">提交</el-button>
+                <el-button type="primary" @click="onSubmit('user')">提交</el-button>
                 <el-button @click="onCancel">取消</el-button>
               </el-row>
             </el-form>
@@ -42,11 +42,25 @@
 </template>
 
 <script>
-import {getUserById, register} from '@/api/user'
+import { register} from '@/api/user'
 
 export default {
   name: 'addUser',
   data(){
+    const validateUsername = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('用户名不能为空'))
+      } else {
+        callback()
+      }
+    };
+    const validatePassword = (rule, value, callback) => {
+      if (value.length < 5) {
+        callback(new Error('密码不能小于5个字符'))
+      } else {
+        callback()
+      };
+    }
     return{
      user:{
        username:'Lovecraft',
@@ -55,11 +69,21 @@ export default {
        phoneNumber: '123456789',
        permissionList:[],
      },
+      userRules: {
+        username: [
+          { required: true, trigger: 'blur', validator: validateUsername },
+        ],
+        password: [
+          { required: true, trigger: 'blur', validator: validatePassword },
+        ],
+      },
     }
   },
   methods:{
-    onSubmit:function() {
-      //检查用户名是否可用
+    onSubmit:function(user) {
+      this.$refs[user].validate((valid) => {
+        if (valid) {
+          //检查用户名是否可用
           register({
               userid: this.user.username,
               nickname: this.user.name,
@@ -69,22 +93,20 @@ export default {
             }
           ).then((res) => {
             console.log(res)
-            if(res.code === 0){
-              this.$notify.success({
-                title: '提示',
-                message: '用户添加成功',
-              })
-              this.$router.push({name: 'addUser'})
-            }else{
-              this.$notify.error({
-                title: '提示',
-                message: '用户名已被占用',
-              })
-            }
+
           }).catch((err) => {
-              console.log(err)
-    })
-    },
+            console.log(err)
+          })
+        }else {
+          this.$notify.error({
+            title: "提示",
+            message: "提交失败"
+          })
+          return false;
+        }
+      }
+      )}
+    ,
 
     onCancel: function(){
       this.$router.push({name:'userManage'})
